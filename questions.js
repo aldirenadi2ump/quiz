00efac9fc509ab75aff7b12deec8aa692647efe8
aldirenadi2ump <109925739+aldirenadi2ump@users.noreply.github.com>
2022-09-10 04:31:03 +0700

    quiz
    quiz des

---
+++ questions.js
@@ -0,0 +1,122 @@
+//creating an array and passing the number, questions, and answer
+let questions = [
+    {
+        numb: 1,
+        question: "Yang dimaksud dengan anak stunting adalah ?",
+        answer: "Anak kerdil/pendek",
+        options: [
+            "Anak kerdil/pendek",
+            "Anak lahir premature",
+            "Anak tinggi",
+            "Anak kelebihan berat badan"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Yang dimaksud dengan ASI esklusif adalah ?",
+        answer: "Pemberian ASI dengan MPASI sejak usia 6 bulan",
+        options: [
+            "Pemberian ASI saja sampai bayi usia 6 bulan",
+            "Pemberian ASI dengan susu formula sampai 6 bulan",
+            "Pemberian ASI dengan MPASI sejak usia 6 bulan",
+            "Pemberian susu formula selama 6 bulan"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "Yang dimaksud dengan IMD adalah ? ",
+        answer: "Inisiasi menyusui dini <1 jam setelah bayi lahir",
+        options: [
+            "Inisiasi makan dini sejak usia bayi 5 bulan",
+            "Inisiasi menyapih dini sejak usia 4 bulan",
+            "Inisiasi menyapih dini sejak usia 5 bulan ",
+            "Inisiasi menyusui dini <1 jam setelah bayi lahir"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "Dibawah ini yang merupakan sumber protein adalah ?",
+        answer: "Ikan & tahu",
+        options: [
+            "Susu ",
+            "Kentang & roti",
+            "Ikan & tahu",
+            "Minyak kelapa"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "ASI dapat diberikan sampai usia ? ",
+        answer: "6 bulan",
+        options: [
+            "3 bulan",
+            "6 bulan",
+            "8 bulan",
+            "1 tahun"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "MPASI dapat mulai diberikan pada usia ? ",
+        answer: "6 bulan",
+        options: [
+            "5 bulan",
+            "6 bulan",
+            "8 bulan",
+            "9 bulan"
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "Dibawah ini panduan gizi seimbang yang benar adalah ",
+        answer: "Piringku",
+        options: [
+            "Piringku",
+            "Tumpeng gizi seimbang",
+            "4 sehat 5 sempurna",
+            "A dan B benar"
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "Pada tumpeng/piramida gizi seimbang komposisi/porsi yang paling sedikit adalah ",
+        answer: "Karbohidrat",
+        options: [
+            "Sayur",
+            "Gula, garam, minyak",
+            "Protein",
+            "Karbohidrat"
+        ]
+    },
+
+    {
+        numb: 9,
+        question: "Apakah hal yang harus dilihat untuk deteksi dini stunting pada balita ?",
+        answer: "A, B, C benar",
+        options: [
+            "Berat badan dan usia",
+            "Tinggi badan dan usia",
+            "Berat badan dan tinggi badan",
+            "A, B, C benar"
+        ]
+    },
+
+    {
+        numb: 10,
+        question: "Dibawah ini merupakan manfaat dari ASI esklusif adalah ? ",
+        answer:  "Semua benar",
+        options: [
+            "Melindungi bayi terhadap infeksi",
+            "Biaya lebih murah dibanding dengan susu formula",
+            "Mengandung zat gizi yang lengkap bagi bayi",
+            "Semua benar"
+        ]
+    },    
+
+];
